1565318d3bf8f63afa8160988f3cb8a6adc2eabc
Farida Kabir <Jummaibakoji@Reedahs-MacBook-Pro.local>
2017-12-21 17:39:47 +0100

    modified db credentials

--- dbconnection.php
+++ dbconnection.php
@@ -1,7 +1,7 @@
 <?php 
 $host = "aussicouncil-production.c9pbg7so34fm.us-west-2.rds.amazonaws.com";
-$username = "root";
-$password = "root";
+$username = "pickin";
+$password = "Pickcrm#3@1s";
 $database = "pickin";
 
 $connection = new mysqli ($host, $username, $password, $database);
